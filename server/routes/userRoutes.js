082651385308c24e0506473599a616361285ec40
Zufan-Hadgu <zufanhadgu1@gmail.com>
2025-05-06 23:37:44 +0300

    user profile update backend api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,18 @@
 import express from 'express'
-import { getUserData } from '../controllers/auth/userController.js';
+import { getUserData ,updateProfile} from '../controllers/auth/userController.js';
 import userAuth from '../middleware/userAuth.js';
+import upload from '../middleware/uploadMiddleware.js'
+ 
 
 
 const userRouter = express.Router();
-userRouter.get('/data', userAuth, getUserData)
+userRouter.get('/data',userAuth,getUserData)
+ 
+userRouter.put('/profile/:id',upload.single('image'), (req, res) => {
+    
+   
+    updateProfile(req, res);
 
-export default userRouter;
+});
+
+export default userRouter;``
