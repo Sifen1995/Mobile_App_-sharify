a6309f7ecd283e0e109948c9503665cbfc78fbd9
sifen getachew <sifenmesfin10@gmail.com>
2025-05-06 11:08:35 -0700

    login and register bug fixed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,12 @@
 import express from 'express'
-import { login, loginAdmin, logout, register, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
-import userAuth from '../middleware/userAuth.js';
- 
+import { login, logout, register,deleteAccount } from '../controllers/auth/authController.js';
 
+import userAuth from '../middleware/userAuth.js'
 const authRouter = express.Router();
 authRouter.post('/register',register)
 authRouter.post('/login',login)
-authRouter.post('/admin-login',loginAdmin)
 authRouter.post('/logout',logout)
-authRouter.post('/send-verify-otp',userAuth,sendVerifyOtp)
-authRouter.post('/verify-account',userAuth,verifyEmail)
-authRouter.post('/send-reset-otp',sendResetOtp)
-authRouter.post('/reset-password',resetPassword)
+authRouter.delete("/delete-account", userAuth, deleteAccount);
+
 
 export default authRouter;
