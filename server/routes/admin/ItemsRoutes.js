d67ba40a50eaa6d17be3ff8e9aec5c878490fce7
Zufan-Hadgu <zufanhadgu1@gmail.com>
2025-05-06 22:27:27 +0300

    Admin REST Api has been implementd

---
+++ server/routes/admin/ItemsRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import isAdmin from '../../middleware/isAdmin.js';
+import upload from '../../middleware/uploadMiddleware.js'
+import { addItem, handleUpload,deleteItem,updateItem ,getAllItems,getStatistics} from '../../controllers/admin/ItemController.js';
+import userAuth from '../../middleware/userAuth.js';
+
+ 
+
+const Adminrouter = express.Router();
+
+// Adminrouter.post("/upload-Image", userAuth, isAdmin, upload.single('image'), handleUpload);
+Adminrouter.post('/add-Item', upload.single('image'),addItem);
+Adminrouter.delete('/delete-item/:id', userAuth, deleteItem);
+Adminrouter.put('/update-item/:id', upload.single('image'), updateItem);
+Adminrouter.get('/items', userAuth,getAllItems);
+Adminrouter.get("/statistics", getStatistics);
+
+
+export default Adminrouter;
