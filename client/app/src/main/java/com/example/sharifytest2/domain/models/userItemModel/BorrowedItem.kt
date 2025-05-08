f7330c483a4a9ce592dc0cdfbfc9f3d4765fbd6c
Zufan-Hadgu <zufanhadgu1@gmail.com>
2025-05-08 22:40:33 +0300

    borrow model

---
+++ client/app/src/main/java/com/example/sharifytest2/domain/models/userItemModel/BorrowedItem.kt
@@ -0,0 +1,10 @@
+ppackage com.example.sharifytest2.domain.models.userItemModel
+
+
+data class BorrowedItem(
+    val id: String,
+    val image: String,
+    val name: String,
+    val smalldescription: String,
+    val note: String
+)
