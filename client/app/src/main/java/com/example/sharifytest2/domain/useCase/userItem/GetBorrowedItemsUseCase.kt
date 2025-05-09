f04c3002ac4519cccfeb11908733fc881ef534e5
Metikejelcha <metike400@gmail.com>
2025-05-09 07:51:05 -0700

    item-api

--- client/app/src/main/java/com/example/sharifytest2/domain/useCase/userItem/GetBorrowedItemsUseCase.kt
+++ client/app/src/main/java/com/example/sharifytest2/domain/useCase/userItem/GetBorrowedItemsUseCase.kt
@@ -1,12 +1,15 @@
 package com.example.sharifytest2.domain.useCase.userItem
 
+
+
+import com.example.sharifytest2.domain.models.userItemModel.BorrowedItem
 import com.example.sharifytest2.domain.repository.ItemRepository
 import javax.inject.Inject
 
-class RemoveBorrowedItemUseCase @Inject constructor(
+class GetBorrowedItemsUseCase @Inject constructor(
     private val itemRepository: ItemRepository // ✅ Inject repository
 ) {
-    suspend fun execute(itemId: String): Boolean {
-        return itemRepository.removeBorrowedItem(itemId) // ✅ Forward note update request
+    suspend fun execute(): List<BorrowedItem> {
+        return itemRepository.getBorrowedItems() // ✅ Fetch borrowed items
     }
 }
